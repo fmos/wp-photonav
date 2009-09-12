00859fd0c6c336a95d4e077da26b52fb04d7f964
fmos <fmos@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-09-12 18:38:25 +0000

    Added media button for the editor which opens a simple helper for the shortcode generation.
    git-svn-id: https://plugins.svn.wordpress.org/wp-photonav/trunk@154107 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -5,6 +5,7 @@ Tags: photo, picture, panorama, photonav, jquery, scroll
 Requires at least: 2.8
 Tested up to: 2.8
 Stable tag: 0.1
+Text Domain: wp-photonav
 
 Provides a scrolling field without scrollbars for pictures with huge horizontal dimensions. Especially usefull for panorama pictures.
 
@@ -14,14 +15,23 @@ This plugin is a rewrite of a JavaScript snippet called PhotoNav. It provides fo
 
 Suitable panorama pictures can be created e.g. using the [Hugin](http://hugin.sourceforge.net/) panorama photo stitcher.
 
-*Until a first major release, I want to encourage everyone to respond to my call for comments concerning this plugin on the [plugin homepage](http://www.fabianmoser.at/wp-photonav). Although my website is mainly in German, the plugin homepage will be kept in English and I hope, so will your posts.*
-
 == Installation ==
 
 1. Upload the wp-photonav directory to the `/wp-content/plugins/` directory
 2. Activate the plugin through the Plugins menu in WordPress
 3. Upload a panorama picture and note the dimensions
 4. Include PhotoNav in your post by entering `[photonav id='panorama' url='/wp-content/uploads/2009/07/panorama.jpg' container_width=400 photo_width=3000 height=200]`. Remember to use **unique** id strings and note that the image will be cropped (not scaled) to the given sizes.
+
+== Version History ==
+
+= Version 0.2 =
+* Changes php source structure to object orientation
+* Panorama media button as a simple shortcode helper
+* I18n
+
+= Version 0.1 =
+* Initial release.
+* Shortcode ability
 
 == Frequently Asked Questions ==
 
--- wp-photonav.php
+++ wp-photonav.php
@@ -1,84 +1,222 @@
-<?php
+<?php 
 /*
-Plugin Name: WP-PhotoNav
-Plugin URI: http://www.fabianmoser.at/wp-photonav
-Description: Provides a scrolling field without scrollbars for huge pictures. Especially usefull for panorama pictures.
-Version: 0.1
-Author: Fabian Moser
-Author URI: http://www.fabianmoser.at
-*/
+ Plugin Name: WP-PhotoNav
+ Plugin URI: http://www.fabianmoser.at/wp-photonav
+ Description: Provides a scrolling field without scrollbars for huge pictures. Especially usefull for panorama pictures.
+ Version: 0.2
+ Author: Fabian Moser
+ Author URI: http://www.fabianmoser.at
+ */
 
-/*  Copyright 2009  Fabian Moser  (email : e-mailÄTfabianmoser.at)
+if (!class_exists('PhotoNav')) {
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
-
-function photonav_init() {
-	$baseDir = '/' . PLUGINDIR . '/wp-photonav';
-	wp_enqueue_script('photonav_script', $baseDir . '/wp-photonav.js', array('jquery'), '0.1');
-	wp_enqueue_style('photonav_style', $baseDir . '/wp-photonav.css', array(), '0.1');
-}
-
-add_action('init', 'photonav_init');
-
-function photonav_add_stylesheet() {
-	wp_enqueue_style('photonav_style');
-}
-
-add_action('wp_print_styles', 'photonav_add_stylesheet');
-
-function photonav_shortcode($atts) {
-	extract(shortcode_atts(array(
-			'id' => 'undefined',
-			'url' => '',
-			'container_width' => 100,
-			'photo_width' => 200,
-			'height' => 100,
-			'debugging' => 0,
-			), $atts));
-	$template_photonav_part1 = 
-			"<div class='photonav'>\n" .
-			"	<div class='container' style='display: none; width: %PHOTONAV_CONTAINERWIDTH%px;' id='%PHOTONAV_ID%'>\n" .
-			"		<div class='photo' style='width: %PHOTONAV_PHOTOWIDTH%px; height: %PHOTONAV_HEIGHT%px; background-image: url(%PHOTONAV_URL%)'></div>\n" .
-			"	</div>\n" .
-			"	<br /><br />\n" .
-			"	<div style='display: ";
-	$template_photonav_part2 = ";'>\n" .
-			"		<strong>Debug info:</strong><br />\n" .
-			"		<div id='status'>&nbsp;</div>\n" .
-			"	</div>\n" .
-			"	<script type='text/javascript'><!--\n" .
-			"		document.getElementById('%PHOTONAV_ID%').style.display = 'block';\n" .
-			"		var %PHOTONAV_ID% = new PhotoNav('%PHOTONAV_ID%', %PHOTONAV_CONTAINERWIDTH%, %PHOTONAV_PHOTOWIDTH%, %PHOTONAV_DEBUGGING%);\n" .
-			"	// --></script>\n" .
-			"</div>\n";
-	$template_photonav = '';
-	if ($debugging == 1) {
-		$template_photonav = $template_photonav_part1 . "block" . $template_photonav_part2;
-	} else {
-		$template_photonav = $template_photonav_part1 . "none" . $template_photonav_part2;
+class PhotoNav {
+	
+	function init() {
+	    $baseDir = '/'.PLUGINDIR.'/wp-photonav';
+	    wp_enqueue_script('photonav_script', $baseDir.'/wp-photonav.js', array('jquery'), '0.1');
+		wp_enqueue_style('photonav_style', $baseDir.'/wp-photonav.css', array(), '0.1');
+	    $this->register_fullscreen_media_button();
 	}
-	$template_photonav = str_replace("%PHOTONAV_ID%", $id, $template_photonav);
-	$template_photonav = str_replace("%PHOTONAV_URL%", $url, $template_photonav);
-	$template_photonav = str_replace("%PHOTONAV_CONTAINERWIDTH%", $container_width, $template_photonav);
-	$template_photonav = str_replace("%PHOTONAV_PHOTOWIDTH%", $photo_width, $template_photonav);
-	$template_photonav = str_replace("%PHOTONAV_HEIGHT%", $height, $template_photonav);
-	$template_photonav = str_replace("%PHOTONAV_DEBUGGING%", $debugging, $template_photonav);
-	return $template_photonav;
+	
+	// Registers the TinyMCE plugin which renders the media button in fullscreen mode
+	function register_fullscreen_media_button() {
+	    // Don't bother doing this stuff if the current user lacks permissions
+	    if (!current_user_can('edit_posts') && !current_user_can('edit_pages'))
+	        return;
+	        
+	    // Add only in Rich Editor mode
+	    if (get_user_option('rich_editing') == 'true') {
+	        add_filter('mce_external_plugins', array(&$this, 'register_tinymce_plugin'));
+	        add_filter('mce_buttons', array(&$this, 'register_tinymce_button'));
+	    }
+	}
+
+	// Callback function for the TinyMCE plugin
+	function register_tinymce_button($buttons) {
+	    array_push($buttons, 'separator', 'photonav');
+	    return $buttons;
+	}
+
+	// Callback function for the TinyMCE plugin
+	function register_tinymce_plugin($plugin_array) {
+	    $plugin_array['photonav'] = WP_PLUGIN_URL.'/wp-photonav/editor_plugin.js';
+	    return $plugin_array;
+	}
+
+	// Adds a media button above the editor in normal (non-fullscreen) mode
+	function add_media_button() {
+		global $post_ID, $temp_ID;
+		$uploading_iframe_ID = (int) (0 == $post_ID ? $temp_ID : $post_ID);
+		$media_upload_iframe_src = "media-upload.php?post_id=$uploading_iframe_ID";
+		$photonav_upload_iframe_src = apply_filters('photonav_upload_iframe_src', "$media_upload_iframe_src&amp;type=photonav&amp;tab=type_url");
+		$photonav_title = 'photonav.add_photonav';
+		$out = "<a href='{$photonav_upload_iframe_src}&amp;TB_iframe=true' id='add_photonav' class='thickbox' title='$image_title' onclick='return false;'><img src='".WP_PLUGIN_URL."/wp-photonav/media-button.gif' alt='$image_title' /></a>";
+		print($out);
+	}
+	
+	// Media upload handler
+	function media_upload_photonav() {
+		$errors = array();
+		$id = 0;
+	
+		if ( isset($_POST['html-upload']) && !empty($_FILES) ) {
+			// Upload File button was clicked
+			$id = media_handle_upload('async-upload', $_REQUEST['post_id']);
+			unset($_FILES);
+			if ( is_wp_error($id) ) {
+				$errors['upload_error'] = $id;
+				$id = false;
+			}
+		}
+	
+		if ( !empty($_POST['insertonlybutton']) ) {
+			if ( !empty($_POST['insertonly']['id']) ) {
+				$id = stripslashes( htmlspecialchars ($_POST['insertonly']['id'], ENT_QUOTES));
+			}
+			$url = $_POST['insertonly']['url'];
+			if ( !empty($url) && !strpos($url, '://') ) {
+				$url = "http://$url";
+			}
+
+			$extras = '';
+			if ( !empty($_POST['insertonly']['containerwidth']) && intval($_POST['insertonly']['containerwidth']) ) {
+				$extras .= ' container_width='.stripslashes( htmlspecialchars ($_POST['insertonly']['containerwidth'], ENT_QUOTES));
+			}
+			if ( !empty($_POST['insertonly']['photowidth']) && intval($_POST['insertonly']['photowidth']) ) {
+				$extras .= ' photo_width='.stripslashes( htmlspecialchars ($_POST['insertonly']['photowidth'], ENT_QUOTES));
+			}
+			if ( !empty($_POST['insertonly']['height']) && intval($_POST['insertonly']['height']) ) {
+				$extras .= ' height='.stripslashes( htmlspecialchars ($_POST['insertonly']['height'], ENT_QUOTES));
+			}
+
+			if ( !empty($id) && !empty($url) ) {
+				$html  = "[photonav id='$id' url='$url'$extras]";
+			}			
+			
+			return media_send_to_editor($html);
+		}
+	
+		if ( !empty($_POST) ) {
+			$return = media_upload_form_handler();
+	
+			if ( is_string($return) )
+				return $return;
+			if ( is_array($return) )
+				$errors = $return;
+		}
+	
+		if ( isset($_POST['save']) ) {
+			$errors['upload_notice'] = __('Saved.');
+			return media_upload_gallery();
+		}
+	
+		if ( isset($_GET['tab']) && $_GET['tab'] == 'type_url' )
+			return wp_iframe( 'media_upload_type_url_form', 'photonav', $errors, $id );
+	
+		// The upload tab is deactivated and there is no upload form available, only the URL form
+		return;
+	}
+	
+	// Hide the upload tab for photonav dialogs
+	function remove_type_tab($tabs) {
+		if ( isset($_REQUEST['type']) && ($_REQUEST['type'] == 'photonav') ) {
+			unset($tabs['type']);
+		}
+		return $tabs;
+	}
+
+	// Parses the shortcode and its parameters and inserts the actual html code
+	function parse_shortcode($atts) {
+	    extract(shortcode_atts(array('id'=>'undefined', 'url'=>'', 'container_width'=>100, 'photo_width'=>200, 'height'=>100, 'debugging'=>0, ), $atts));
+		$template_photonav = '';
+	    if ($debugging == 1) {
+	        $template_photonav = 'block';
+	    } else {
+	        $template_photonav = 'none';
+	    }
+	    $template_photonav = "<div class='photonav'><div class='container' style='display: none; width: %PHOTONAV_CONTAINERWIDTH%px;' id='%PHOTONAV_ID%'><div class='photo' style='width: %PHOTONAV_PHOTOWIDTH%px; height: %PHOTONAV_HEIGHT%px; background-image: url(%PHOTONAV_URL%)'></div></div><br /><br /><div style='display: ".$template_photonav.";'><strong>Debug info:</strong><br /><div id='status'>&nbsp;</div></div><script type='text/javascript'>document.getElementById('%PHOTONAV_ID%').style.display = 'block';var %PHOTONAV_ID% = new PhotoNav('%PHOTONAV_ID%', %PHOTONAV_CONTAINERWIDTH%, %PHOTONAV_PHOTOWIDTH%, %PHOTONAV_DEBUGGING%);</script></div>";
+	    $template_photonav = str_replace("%PHOTONAV_ID%", $id, $template_photonav);
+	    $template_photonav = str_replace("%PHOTONAV_URL%", $url, $template_photonav);
+	    $template_photonav = str_replace("%PHOTONAV_CONTAINERWIDTH%", $container_width, $template_photonav);
+	    $template_photonav = str_replace("%PHOTONAV_PHOTOWIDTH%", $photo_width, $template_photonav);
+	    $template_photonav = str_replace("%PHOTONAV_HEIGHT%", $height, $template_photonav);
+	    $template_photonav = str_replace("%PHOTONAV_DEBUGGING%", $debugging, $template_photonav);
+	    return $template_photonav;
+	}
+
+} // end PhotoNav class
+
+} // end class_exists check
+
+if (class_exists('PhotoNav')) {
+	$photonav = new PhotoNav();
 }
 
-add_shortcode('photonav', 'photonav_shortcode');
+load_plugin_textdomain('wp-photonav', $baseDir, 'wp-photonav');
+
+function type_url_form_photonav() { 
+	return ' 
+	<table class="describe"><tbody>
+		<tr>
+			<th valign="top" scope="row" class="label">
+				<span class="alignleft"><label for="insertonly[id]">' . __('ID', 'wp-photonav') . '</label></span>
+				<span class="alignright"><abbr title="required" class="required">*</abbr></span>
+			</th>
+			<td class="field">
+				<input id="insertonly[id]" name="insertonly[id]" value="" type="text">
+			</td>
+		</tr>	
+		<tr>
+			<th valign="top" scope="row" class="label">
+				<span class="alignleft"><label for="insertonly[url]">' . __('Panorama URL', 'wp-photonav') . '</label></span>
+				<span class="alignright"><abbr title="required" class="required">*</abbr></span>
+			</th>
+			<td class="field"><input id="insertonly[url]" name="insertonly[url]" value="" type="text"></td>
+		</tr>			
+		<tr>
+			<th valign="top" scope="row" class="label">
+				<span class="alignleft"><label for="insertonly[containerwidth]">' . __('Container width', 'wp-photonav') . '</label></span>
+				<span class="alignright"><abbr title="required" class="required">*</abbr></span>
+			</th>
+			<td class="field">
+				<input id="insertonly[containerwidth]" name="insertonly[containerwidth]" value="" type="text" class="halfpint">
+			</td>
+		</tr>
+		<tr>
+			<th valign="top" scope="row" class="label">
+				<span class="alignleft"><label for="insertonly[photowidth]">' . __('Photo width', 'wp-photonav') . '</label></span>
+				<span class="alignright"><abbr title="required" class="required">*</abbr></span>
+			</th>
+			<td class="field">
+				<input id="insertonly[photowidth]" name="insertonly[photowidth]" value="" type="text" class="halfpint">
+			</td>
+		</tr>
+		<tr>
+			<th valign="top" scope="row" class="label">
+				<span class="alignleft"><label for="insertonly[height]">' . __('Height', 'wp-photonav') . '</label></span>
+				<span class="alignright"><abbr title="required" class="required">*</abbr></span>
+			</th>
+			<td class="field">
+				<input id="insertonly[height]" name="insertonly[height]" value="" type="text" class="halfpint">
+			</td>
+		</tr>
+		<tr>
+			<td></td>
+			<td>
+				<input type="submit" class="button" name="insertonlybutton" value="' . esc_attr__('Insert into Post') . '" />
+			</td>
+		</tr>
+	</tbody></table>
+	';
+}
+
+if (isset($photonav)) {
+	add_action('init', array(&$photonav, 'init'));
+	add_action('media_buttons', array(&$photonav, 'add_media_button'), 20);
+	add_action('media_upload_photonav', array(&$photonav, 'media_upload_photonav'));
+	add_filter('media_upload_tabs', array(&$photonav, 'remove_type_tab'));
+	add_shortcode('photonav', array(&$photonav, 'parse_shortcode'));
+}
 
 ?>
