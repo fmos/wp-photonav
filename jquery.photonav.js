c51dfc57982f4747670b63069a3dd4a0b962e3f8
fmos <fmos@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-06-02 12:21:04 +0000

    Release 1.0 featuring a rewrite of drag modes and other changes
    git-svn-id: https://plugins.svn.wordpress.org/wp-photonav/trunk@392164 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- jquery.photonav.js
+++ jquery.photonav.js
@@ -1,11 +1,11 @@
-// vim: ai ts=4 sts=4 et sw=4
-
 /*
- * 	PhotoNavigation for WordPress
+ * 	PhotoNavigation for WordPress "WP-PhotoNav"
  * 	
- * 	Version 0.8
- * 	Date: 10-11-21
- * 
+ * 	Version: 1.0
+ * 	Date: 11-06-01
+ *  Author: Fabian Moser
+ *  Author URI: http://fabianmoser.at
+ *  License: GPL version 2 or later - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  */
 
 /*  Credits:
@@ -13,141 +13,208 @@
  * 	PhotoNavigation the jQuery version
  * 	A Javascript Module by Gaya Kessler
  * 	Version 1.0
- * 	Date: 09-04-09
- * 
+ * 	Date: 09-04-09 
  */
 
-(function ($) {
-    $.fn.photoNav = function(settings) {
-        var config = {
-            mode: 'move',
-            popup: 'none',
-            animate: '0'
-        };
+(function($) {$.fn.photoNav = function(settings) {
+	var config = {
+			id : false,
+			mode : 'move',
+			popup : 'none',
+			animate : '0'
+	};
 
-        if (settings) $.extend(config, settings);
+	if (settings)
+		$.extend(config, settings);
 
-        function initMove(container) {
-            container.bind('mousemove', function (event) {
-                var image = $(this).find('img');
-                var offset = $(this).offset();
-                var curX = (this.offsetWidth - image[0].offsetWidth) /
-                (this.offsetWidth / (event.pageX - offset.left));
-                var curY = (this.offsetHeight - image[0].offsetHeight) /
-                (this.offsetHeight / (event.pageY - offset.top));
-                var imageWrapper = $(this).find('.image');
-                imageWrapper.stop();
-                imageWrapper.css('left', curX > 0 ? 0 : curX);
-                imageWrapper.css('top',  curY > 0 ? 0 : curY);
-            });
-        }
+	function PhotoNav(elem) {
+		var self = this;
 
-        function initDrag(container) {
-            var image = container.find('img');
-            var constraints = [0,0,0,0];
-            constraints[0] = container.offset().left - image.width()  + container.width();
-            constraints[1] = container.offset().top  - image.height() + container.height();
-            constraints[2] = container.offset().left;
-            constraints[3] = container.offset().top;
-            container.find('.image').draggable({
-                containment: constraints,
-                start: function() {
-                    $(this).stop();
-                }
-            });
-        }
+		var inline = elem.children('.container');
+		var image = inline.find('.image');
 
-        function initDrag360(container) {
-            var image = container.find('img');
-            var constraints = [0,0,0,0];
-            constraints[0] = container.offset().left - image.width()  - container.width() ;
-            constraints[1] = container.offset().top  - image.height() + container.height();
-            constraints[2] = container.offset().left + image.width();
-            constraints[3] = container.offset().top;
-            var imageWrapper = container.find('.image');
-            imageWrapper.css('width', image.width() + container.width() + 2);
-            imageWrapper.draggable({
-                containment: constraints,
-                start: function() {
-                    $(this).stop();
-                },
-                drag: function(e, ui) {
-                    var newleft = ui.position.left % image.width();
-                    if (newleft > 0) {
-                        newleft -= image.width();
-                    }
-                    ui.position.left = newleft;
-                }
-            });
-        }
+		this.getImageWidth = function() {
+			return image[0].scrollWidth;
+		};
+		this.getImageHeight = function() {
+			return image[0].scrollHeight;
+		};
 
-        // Calls the appropriate init method above depending on the mode parameter.
-        function initMode(container, mode) {
-            if (mode == 'move') {
-                initMove(container);
-            }
-            else if (mode == 'drag') {
-                initDrag(container);
-            }
-            else if (mode == 'drag360') {
-                initDrag360(container);
-            }
-        }
+		this.updateContent = function(content, width) {
+			// Copy the image size to all content DIVs
+			if (width == undefined) {
+				width = self.getImageWidth();
+			}
+			content.css('height', self.getImageHeight());
+			content.css('width', width);
+		};
 
-        // Initializes the ColorBox popup.
-        function initColorbox(image, popup, mode) {
-            var container = popup.children('.container');
-            var innerImage = container.children('.image');
-            image.colorbox({
-                maxWidth: '100%',
-                maxHeight: '100%',
-                inline: true,
-                href: popup[0],
-                onOpen: function () {
-                    container.css('width', 'auto');
-                    container.css('height', image.height());
-                    innerImage.css('background-repeat', 'repeat');
-                    innerImage.css('height', image.height());
-                },
-                onComplete: function () {
-                    var innerHeight = popup.parent().innerHeight();
-                    if (innerHeight < popup.height()) {
-                        container.css('height', innerHeight);
-                    }
-                    initMode(container, mode);
-                }
-            });
-        }
+		this.initMove = function(container) {
+			var content = container.find('.content');
+			function updateMove() {
+				var iw = self.getImageWidth(), cw = container.width();
+				var ih = self.getImageHeight(), ch = container.height();
+				self.updateContent(content);
+				content.css('left', Math.min(0,(cw-iw)/2));
+				content.css('top', Math.min(0,(ch-ih)/2));
+				return [0, cw-iw];
+			};
+			var anirange = updateMove();
+			container.mousemove(function(event) {
+				var offset = $(this).offset();
+				var curX = (event.pageX - offset.left) * (1 - self.getImageWidth() / this.offsetWidth);
+				var curY = (event.pageY - offset.top) * (1 - self.getImageHeight() / this.offsetHeight);
+				content.stop();
+				content.css('left', curX > 0 ? 0 : curX);
+				content.css('top', curY > 0 ? 0 : curY);
+			});
+			content.children('.image').load(updateMove);
+			return anirange;
+		};
 
-        function createPhotoNav(photonav, mode, popup_type, animate) {
-            var inline = photonav.children('.container');
-            var popup = photonav.find('.popup');
+		this.initDrag = function(container) {
+			var content = container.find('.content');
+			var wrapper = content.parent();
+			if (wrapper.attr('class') != 'dragconstraint') {
+				wrapper = content.wrap('<div class="dragconstraint" />').parent();
+			}
+			function updateDrag() {
+				self.updateContent(content);
+				var iw = self.getImageWidth(), cw = container.width();
+				var ww = 2*iw - cw;
+				var ih = self.getImageHeight(), ch = container.height();
+				var wh = 2*ih - ch;
+				wrapper.width(ww);
+				wrapper.css('margin-left', (cw-ww)/2);
+				wrapper.height(wh);
+				wrapper.css('margin-top', (ch-wh)/2);
+				content.css('left', Math.max(0,(iw-cw)/2));
+				content.css('top', Math.max(0,(ih-ch)/2));
+				return [iw-cw, 0];
+			}
+			var anirange = updateDrag();
+			content.draggable({
+				start : function() {
+					$(this).stop(); // Stop animation
+				},
+				containment : 'parent'
+			});
+			content.children('.image').load(updateDrag);
+			return anirange;
+		};
 
-            inline.css('display', 'block'); // show PhotoNav instance
-            photonav.find('.image').each(function () {
-                $(this).css('height', $(this).find('img').height());
-            });
-            inline.find('.image').each(function () {
-                var minLeft = inline.offset().left - $(this).find('img').width()  + inline.width()
-                if (animate == '1') {
-                    $(this).css('left', 0);
-                    $(this).animate({
-                        left: minLeft
-                    }, -10*minLeft, 'linear');
-                }
-            });
+		this.initDrag360 = function(container) {
+			var content = container.find('.content');
+			var wrapper = content.parent();
+			if (wrapper.attr('class') != 'dragconstraint') {
+				wrapper = content.wrap('<div class="dragconstraint" />').parent();
+			}
+			function updateDrag360() {
+				var iw = self.getImageWidth(), cw = container.width();
+				var ww = 2*iw + cw + 4;
+				var ih = self.getImageHeight(), ch = container.height();
+				var wh = 2*ih - ch;
+				wrapper.width(ww);
+				wrapper.css('margin-left', (cw-ww)/2);
+				wrapper.height(wh);
+				wrapper.css('margin-top', (ch-wh)/2);
+				self.updateContent(content, iw + cw + 2);
+				content.css('left', Math.max(0,(iw+cw)/2));
+				content.css('top', Math.max(0,(ih-ch)/2));
+				return [iw, cw];
+			}
+			var anirange = updateDrag360();
+			content.draggable({
+				start : function() {
+					$(this).stop();
+				},
+				drag : function(e, ui) {
+					var iw = self.getImageWidth();
+					var newleft = ui.position.left;
+					if (newleft > iw) {
+						$(this).data('draggable').offset.click.left += iw;
+					} else if (newleft < 1) {
+						$(this).data('draggable').offset.click.left -= iw;
+					}
+				},
+				containment : 'parent'
+			});
+			content.children('.image').load(updateDrag360);
+			return anirange;
+		};
 
-            initMode(inline, mode);
+		// Calls the appropriate init method above depending on the mode
+		// parameter.
+		this.initMode = function(container, mode) {
+			if (mode == 'move') {
+				return self.initMove(container);
+			} else if (mode == 'drag') {
+				return self.initDrag(container);
+			} else if (mode == 'drag360') {
+				return self.initDrag360(container);
+			}
+		};
 
-            if (popup_type == 'colorbox') {
-                initColorbox(inline.find('img'), popup, mode);
-            }
-        }
+		// Sets up the animation
+		this.initAnimation = function(container, anirange) {
+			inline.find('.content').each(
+					function() {
+						var image = $(this).find('.image');
+						var minLeft = container.offset().left - self.getImageWidth() + container.width();
+						$(this).css('left', anirange[0]);
+						$(this).animate({
+							left : anirange[1]
+						}, 10 * Math.abs(anirange[1] - anirange[0]), 'linear');
+					});
+		};
 
-        this.each(function() {
-            createPhotoNav($(this), config['mode'], config['popup'], config['animate']);
-        });
+		// Initializes the ColorBox popup.
+		this.initColorbox = function(popup, id, mode) {
+			var popupid = id + '-popup';
+			var container = popup.children('.container');
+			var content = container.find('.content');
+			popup.attr('id', popupid);
+			image.colorbox({
+				maxWidth : '100%',
+				maxHeight : '100%',
+				inline : true,
+				href : '#'+popupid,
+				onOpen : function() {
+					container.css('width', 'auto');
+					container.css('height', self.getImageHeight());
+					content.css('background-repeat', 'repeat');
+					content.css('height', self.getImageHeight());
+				},
+				onComplete : function() {
+					$('#'+popupid).each(function () {
+						var container = $(this).children('.container');
+						var innerHeight = $(this).parent().innerHeight();
+						if (innerHeight < $(this).height()) {
+							container.css('height', innerHeight);
+						}
+						self.initMode(container, mode);
+					});
+				}
+			});
+		};
 
-        return this;
-    };
-})(jQuery);
+		this.init = function(id, mode, popup_type, animate) {
+			inline.css('display', 'block'); // unhide
+			anirange = self.initMode(inline, mode);
+			if (animate == '1')
+				self.initAnimation(inline, anirange);
+			if (popup_type == 'colorbox') {
+				if ($().colorbox) {
+					self.initColorbox(elem.find('.popup'), id, mode);
+				}
+			}
+		};
+	};
+
+	this.each(function() {
+		var photonav = new PhotoNav($(this));
+		photonav.init(config['id'], config['mode'], config['popup'], config['animate']);
+	});
+
+	return this;
+};}(jQuery));
