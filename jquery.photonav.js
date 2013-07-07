f45b1ec0f58e1e1a1882e0af8e644debc3fc17f8
Fabian Stanke <me@fmos.at>
2013-07-07 12:50:21 +0200

    Revert "Update version information"
    This reverts commit 066080e7aaad152527a0d9d7f162211ba837cb21.

--- jquery.photonav.js
+++ jquery.photonav.js
@@ -1,8 +1,8 @@
 /*
  *  PhotoNavigation for WordPress "WP-PhotoNav"
  *
- *  Version: 1.0.2
- *  Date: 13-07-03
+ *  Version: 1.0.1
+ *  Date: 13-03-01
  *  Author: Fabian Stanke
  *  Author URI: http://fmos.at
  *  License: GPL version 2 or later - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
